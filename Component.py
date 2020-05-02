9d141546a9ea5b15960fef6135735ff182172ca6
zipph <dcpulido91@gmail.com>
2020-05-03 01:23:26 +0200

    Added first implementation of component and mqtt iface

---
+++ Component.py
@@ -0,0 +1,37 @@
+import json
+import time
+
+import sys
+sys.path.insert(0, './interfaces')  # noqa
+
+from MqttHandler import MqttHandler
+from abc import ABC, abstractmethod
+
+
+class Component(ABC):
+    def __init__(self, conf):
+        self.conf_ = conf
+        if "comm_iface" not in conf.keys():
+            self.conf_["comm_iface"] = None
+        self.iface_ = MqttHandler(self.conf_["comm_iface"],
+                                  callback=self.proc)
+
+    @abstractmethod
+    def proc(self, msg):
+        pass
+
+    def get_command(self, msg):
+        return msg["payload"]["command"]
+
+    def send_msg(self, msg):
+        if "command" in msg.keys():
+            self.iface_.on_publish(
+                "/DMKernel/Info", json.dumps(msg))
+        else:
+            logging.error("msg not sent due no command on it")
+
+    def start(self):
+        self.iface_.start()
+
+    def stop(self):
+        self.iface_.stop()
